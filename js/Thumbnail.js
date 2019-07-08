bdf489a3ae14611fa7e41cdd9dd7481365091410
Emilie Sabathier <sabathier.emilie@gmail.com>
2019-07-08 21:22:47 +0200

    Create Thumbnail class + array objects thumbnails

---
+++ js/Thumbnail.js
@@ -0,0 +1,18 @@
+
+class Thumbnail {
+    /**
+     * Create the thumbnail with the properties
+     * @param {string} titre 
+     * @param {string} author 
+     * @param {string} date 
+     * @param {string} profession 
+     * @param {string} image 
+     */
+    constructor(titre,author,date,profession,image) {
+        this.title = titre;
+        this.author = author;
+        this.date = date;
+        this.profession = profession;
+        this.image = image;
+    }
+}
---
+++ js/main.js
@@ -0,0 +1,12 @@
+/**
+ * Create empty array
+ * Then, push each object in the array, with the informations.
+ */
+const thumbnailsArray = [];
+thumbnailsArray.push[new Thumbnail('Sed ut perspiciatis','Mme Pérera', '8 juillet 2019', 'documentaliste','https://picsum.photos/200')];
+thumbnailsArray.push[new Thumbnail('Neque porro quisquam','M. Patini', '10 juin 2019', 'professeur de français','https://picsum.photos/200')];
+thumbnailsArray.push[new Thumbnail('At vero eos et accusamus','Mme Sabatini', '26 avril 2019', '','https://picsum.photos/200')];
+thumbnailsArray.push[new Thumbnail('At harum quidem','M. Magionfreddo', '5 mai 2019', 'professeur d\'anglais','https://picsum.photos/200')];
+thumbnailsArray.push[new Thumbnail('At vero eos','Mme Nefes', '31 mars 2019', 'documentaliste','https://picsum.photos/200')];
+thumbnailsArray.push[new Thumbnail('Harum quidem','Mme Bounzy', '14 janvier 2019', '','https://picsum.photos/200')];
+
